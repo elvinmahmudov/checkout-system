c982ea10083ec8bb1997afa1c5c475740dc00627
Elvin Mahmudov <elvin.mahmudov@pashabank.az>
2020-07-29 23:00:05 +0400

    Add item model

---
+++ src/main/java/com/elvinmahmudov/model/Item.java
@@ -0,0 +1,57 @@
+package main.java.com.elvinmahmudov.model;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Item {
+    private String productCode;
+
+    private String name;
+
+    private BigDecimal price;
+
+    public Item(String productCode, String name, BigDecimal price) {
+        this.productCode = productCode;
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getProductCode() {
+        return productCode;
+    }
+
+    public void setProductCode(String productCode) {
+        this.productCode = productCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return Objects.equals(productCode, item.productCode) &&
+                Objects.equals(name, item.name) &&
+                Objects.equals(price, item.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(productCode, name, price);
+    }
+}
